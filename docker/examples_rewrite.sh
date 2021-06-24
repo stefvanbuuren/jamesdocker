d4a16659710dfec41240e536187da878101484cc
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-06-24 16:17:27 +0200

    Streamline public API further
    - Use `/xxx` for in comment lines
    - Change `key` to `upload` for clarity
    - Changes /screeners/growth/man
    - Removes `calc/dscore`
    - Updates custom/man
    - Check everything in examples_rewrite

--- docker/examples_rewrite.sh
+++ docker/examples_rewrite.sh
@@ -1,25 +1,28 @@
-# --> How can I upload data and get a key?
+# --> How can I upload data to server and get a key to it?
 
 # GET: download example data
 curl https://groeidiagrammen.nl/ocpu/library/james/testdata/client3.json -O
 
-# POST: upload local file to groeidiagrammen.nl (oldstyle)
+# POST: upload local file to groeidiagrammen.nl (oldstyle, not recommended)
 curl -X POST -F 'txt=@client3.json' https://groeidiagrammen.nl/ocpu/library/james/R/fetch_loc
 
-# POST: upload local file client3.json to localhost
-curl -X POST -F 'txt=@client3.json' localhost/key
+# POST: upload local file client3.json to localhost (new style)
+curl -X POST -F 'txt=@client3.json' localhost/upload
 
 # POST: upload dataset as a JSON string (assumes jq utility installed)
-dat=$(jq '.' client3.json | jq -sR '.')
-curl localhost/key -d "txt=$dat"
+txt=$(jq '.' client3.json | jq -sR '.')
+curl localhost/upload -d "txt=$txt"
 
-# store key to my data on the server
-key=x0cd14ee778d0fb
+# extract key to upload on the server
+key=x0df32370ff41f4
 echo $key
 
+# GET: R documentation for upload
+curl localhost/upload/man
 
 
-# --> How can I inspect my uploaded data?
+
+# --> How can I inspect my session?
 
 # inspect session of uploaded data
 curl localhost/$key
@@ -28,32 +31,29 @@ curl localhost/$key
 curl localhost/$key/print
 
 # download uploaded and validated data as JSON
-curl localhost/$key/json > fromjames.json
+curl localhost/$key/json > uploaded.json
 
-# inspect validation messages
+# inspect validation messages, if any
 curl localhost/$key/messages
 
 # we can ask: print, json, svg, console, stdout, warnings, messages, info, source, R, files
 
 
 
-# --> Which charts can we make?
+# --> Which charts can I make?
 
-# POST: opencpu session (internal)
-curl -X POST localhost://charts/list
+# POST: JSON list of all chart codes
+curl -X POST localhost/charts/list/json
 
-# POST: JSON list of all chart codes (external)
-curl -X POST localhost://charts/list/json
+# POST: return text table of all chart codes
+curl -X POST localhost/charts/list/print
 
-# POST: table of all chart codes (external)
-curl -X POST localhost://charts/list/print
-
-# GET: R documentation list_charts() (external)
+# GET: R documentation list_charts()
 curl localhost://charts/list/man
 
 
 
-# --> How do we select a specific chart?
+# --> How do I select a specific chart?
 
 # POST: draw empty chart NMBA
 curl localhost://charts/draw -d "chartcode='NMBA'&selector='chartcode'"
@@ -68,18 +68,21 @@ curl localhost://charts/draw/print -d "chartcode='NMBA'&selector='chartcode'"
 # https://stackoverflow.com/questions/8581054/how-do-i-rewrite-a-post-request-from-a-form-to-a-user-friendly-url-with-htaccess
 # curl -X POST localhost://charts/NMBA
 
-# GET: R documentation draw_chart (external)
-curl localhost://charts/draw/man
 
+# --> How do I add data to the chart?
 
+# POST: upload local child data and draw default chart
+curl localhost/charts/draw -d "txt=$txt"
 
-# --> How do we add data to a chart?
+# POST: read child data from storage and draw default chart
+# FAILS
+# curl -X POST localhost/$key/charts/draw
 
-# POST: upload local child data and plot on NMBA (chartcode) (external)
-curl localhost://charts/draw -d "txt=$dat&chartcode='NMBA'&selector='chartcode'"
+# POST: upload local child data and plot on NMBA (chartcode)
+curl localhost/charts/draw -d "txt=$txt&chartcode='NMBA'&selector='chartcode'"
 
-# POST: read child data from storage and plot on NMBA (chartcode) (faster for multiple graphs) (external)
-curl localhost://charts/draw -d "loc='http://localhost/ocpu/tmp/$key/'&chartcode='NMBA'&selector='chartcode'"
+# POST: read child data from storage and plot on NMBA (chartcode) (faster for multiple graphs)
+curl localhost/charts/draw -d "loc='http://localhost/ocpu/tmp/$key/'&chartcode='NMBA'&selector='chartcode'"
 
 # POST: read child data from storage and plot on NMBA - neat URL
 # the nicer way, but it does not work
@@ -90,17 +93,39 @@ curl localhost://charts/draw -d "loc='http://localhost/ocpu/tmp/$key/'&chartcode
 # slightly simplified, but also does not work
 # curl -X POST localhost://$key/charts/draw -d "chartcode='NMBA'&selector='chartcode'"
 
+# GET: R documentation for /charts/draw
+curl localhost/charts/draw/man
+
 
 
 # ---> How do I download the chart?
 
 # download chart, the short path
-cht=x06c34f2abc8170
+cht=x0d5c6128776291
 curl -o mychart1.svg "localhost/$cht/svg?width=8.27&height=11.69" -H 'Cache-Control: max-age=0'
 
 # download chart, the long path (oldstyle)
 curl -o mychart2.svg "localhost/ocpu/tmp/$cht/graphics/1/svglite?width=8.27&height=11.69" -H 'Cache-Control: max-age=0'
 
+# GET: documentation for $cht/svg
+curl localhost://ocpu/library/svglite/man/svglite/text
+
+
+
+## ---> How do I screen the growth of a child for abnormalities?
+
+# POST: upload local child data and screen all outcomes
+curl localhost://screeners/growth -d "txt=$txt"
+
+# POST: read child data from storage and screen all growth parameters (faster)
+curl localhost://screeners/growth -d "loc='http://localhost/ocpu/tmp/$key/'"
+
+# GET: download results from screeners
+scr=x04875911c17e56
+curl -o screen.json "localhost/$scr/json"
+
+# GET: R documentation for screeners/growth
+curl localhost/screeners/growth/man
 
 
 ## ---> How do I obtain a personalised site?
